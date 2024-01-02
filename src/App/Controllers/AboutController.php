575024441a363ff62da4cd8352b5164a6111581c
ggeorgieva <ggeorgieva@GGEORGIEVA_PC>
2024-01-02 15:41:17 +0200

    autholoading functions

--- src/App/Controllers/AboutController.php
+++ src/App/Controllers/AboutController.php
@@ -19,7 +19,8 @@ class AboutController
     public function about()
     {
         echo $this->view->render('about.php', [
-            'title' => 'About'
+            'title' => 'About',
+            'dangerousData' => '<script>alert(123)</script>'
         ]);
     }
 }
--- src/App/bootstrap.php
+++ src/App/bootstrap.php
@@ -4,12 +4,11 @@ declare(strict_types=1);
 
 require __DIR__ . "/../../vendor/autoload.php";
 
-use App\Controllers\{HomeController, AboutController};
 use Framework\App;
+use function App\Config\registerRoutes;
 
 $app = new App();
 
-$app->get('/', [HomeController::class, 'home']);
-$app->get('/about', [AboutController::class, 'about']);
+registerRoutes($app);
 
 return $app;
