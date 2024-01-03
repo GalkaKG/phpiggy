fdb4c800d6b91e9587ba5405266dc6ba525fb3ce
GalkaKG <galina_georgieva_net@abv.bg>
2024-01-03 12:34:53 +0200

    test

--- src/Framework/Container.php
+++ src/Framework/Container.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Framework;
 
-use ReflectionClass;
+use ReflectionClass, ReflectionNamedType;
 use Framework\Exceptions\ContainerException;
 
 class Container
@@ -39,6 +39,12 @@ class Container
             return new $className;
         }
 
+        $dependencies = [];
+
+
+
         dd($params);
     }
 }
+
+//Test
