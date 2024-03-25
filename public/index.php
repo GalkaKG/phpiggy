55a1587641cff6443338003006feb9cc250b5742
GalkaKG <galina_georgieva_net@abv.bg>
2024-03-25 15:05:07 +0200

    Add composer

--- public/index.php
+++ public/index.php
@@ -1,4 +1,5 @@
 <?php
 
-ini_set('memory_limit', '255M');
-echo ini_get('memory_limit');
+$app = include __DIR__ . "/../src/App/bootstrap.php";
+
+$app->run();
---
+++ src/App/bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+include __DIR__ . "/../Framework/App.php";
+
+use Framework\App;
+
+$app = new App();
+
+return $app;
