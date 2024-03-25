22dee852c0e0e1907bab9e28d3d64f7f66331a0b
GalkaKG <galina_georgieva_net@abv.bg>
2024-03-25 16:20:38 +0200

    Add autoloading

--- src/App/bootstrap.php
+++ src/App/bootstrap.php
@@ -2,7 +2,8 @@
 
 declare(strict_types=1);
 
-include __DIR__ . "/../Framework/App.php";
+// include __DIR__ . "/../Framework/App.php";
+require __DIR__ . "/../../vendor/autoload.php";
 
 use Framework\App;
 
