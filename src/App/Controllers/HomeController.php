3fa636cae0c08db52a013525d4ee1d547eb70a6b
GalkaKG <galina_georgieva_net@abv.bg>
2024-03-26 11:23:15 +0200

    Create template engine

--- src/App/Controllers/HomeController.php
+++ src/App/Controllers/HomeController.php
@@ -4,10 +4,21 @@ declare(strict_types=1);
 
 namespace App\Controllers;
 
+use Framework\TemplateEngine;
+use App\Config\Paths;
+
 class HomeController
 {
+    private TemplateEngine $view;
+
+    public function __construct()
+    {
+        $this->view = new TemplateEngine(Paths::VIEW);
+    }
+
     public function home()
     {
+        dd($this->view);
         echo 'home page';
     }
 }
---
+++ src/Framework/TemplateEngine.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Framework;
+
+class TemplateEngine
+{
+    public function __construct(private string $basePath)
+    {
+
+    }
+}
