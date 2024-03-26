37578faff376c389f598f06d28666a9cf9b44a9a
GalkaKG <galina_georgieva_net@abv.bg>
2024-03-26 12:38:15 +0200

    Set partials

--- src/Framework/TemplateEngine.php
+++ src/Framework/TemplateEngine.php
@@ -16,12 +16,17 @@ class TemplateEngine
 
         ob_start();
 
-        include "{$this->basePath}/{$template}";
+        include $this->resolve($template);
 
         $output = ob_get_contents();
 
         ob_end_clean();
 
         return $output;
+    }
+
+    public function resolve(string $path)
+    {
+        return "{$this->basePath}/{$path}";
     }
 }
