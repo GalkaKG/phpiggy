ee2ed1f3b27e892d1f8bed8232ac8fa7d5c2ef97
GalkaKG <galina_georgieva_net@abv.bg>
2024-03-26 13:20:49 +0200

    Separate Routes

--- src/App/bootstrap.php
+++ src/App/bootstrap.php
@@ -6,11 +6,11 @@ declare(strict_types=1);
 require __DIR__ . "/../../vendor/autoload.php";
 
 use Framework\App;
-use App\Controllers\{HomeController, AboutController};
+
+use function App\Config\registerRoutes;
 
 $app = new App();
 
-$app->get('/', [HomeController::class, 'home']);
-$app->get('/about', [AboutController::class, 'about']);
+registerRoutes($app);
 
 return $app;
