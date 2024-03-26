5894fab0e50160ac7e06b5953f892242b5b52e36
GalkaKG <galina_georgieva_net@abv.bg>
2024-03-26 12:52:29 +0200

    Add about page

--- src/App/bootstrap.php
+++ src/App/bootstrap.php
@@ -6,10 +6,11 @@ declare(strict_types=1);
 require __DIR__ . "/../../vendor/autoload.php";
 
 use Framework\App;
-use App\Controllers\HomeController;
+use App\Controllers\{HomeController, AboutController};
 
 $app = new App();
 
 $app->get('/', [HomeController::class, 'home']);
+$app->get('/about', [AboutController::class, 'about']);
 
 return $app;
