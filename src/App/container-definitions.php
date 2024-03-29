ae2235c54f68c4893125d953eb1f2f2ea1b5a972
GalkaKG <galina_georgieva_net@abv.bg>
2024-03-29 10:15:44 +0200

    Creating environment variables

--- src/App/container-definitions.php
+++ src/App/container-definitions.php
@@ -2,11 +2,16 @@
 
 declare(strict_types=1);
 
-use Framework\TemplateEngine;
+use Framework\{TemplateEngine, Database};
 use App\Config\Paths;
 use App\Services\ValidatorService;
 
 return [
-    TemplateEngine::class => fn() => new TemplateEngine(Paths::VIEW),
-    ValidatorService::class => fn() => new ValidatorService()
+    TemplateEngine::class => fn () => new TemplateEngine(Paths::VIEW),
+    ValidatorService::class => fn () => new ValidatorService(),
+    Database::class => fn () => new Database($_ENV['DB_DRIVER'], [
+        'host' => $_ENV['DB_HOST'],
+        'port' => $_ENV['DB_PORT'],
+        'dbname' => $_ENV['NAME']
+    ], $_ENV['DB_USER'], $_ENV['DB_PASS'])
 ];
