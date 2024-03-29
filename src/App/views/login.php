bc475b367a0db0d75f442636fbb84c3049fcc237
GalkaKG <galina_georgieva_net@abv.bg>
2024-03-29 19:00:56 +0200

    Rendering csrf tokn

--- src/App/views/login.php
+++ src/App/views/login.php
@@ -2,6 +2,8 @@
 
 <section class="max-w-2xl mx-auto mt-12 p-4 bg-white shadow-md border border-gray-200 rounded">
     <form method="POST" class="grid grid-cols-1 gap-6">
+        <?php include $this->resolve('partials/_csrf.php'); ?>
+
         <label class="block">
             <span class="text-gray-700">Email address</span>
             <input value="<?php echo e($oldFormData['email'] ?? ''); ?>" name="email" type="email" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" placeholder="john@example.com" />
